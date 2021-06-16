74ea471cb96994560db668118161a6feb63a7d6d
c3budiman <c3budiman@gmail.com>
2021-06-16 07:14:18 +0700

    unit testing example

---
+++ test/login_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterboiler/Pages/Login/Screen/LoginScreen.dart';
+
+void main() {
+  testWidgets(
+    'Render Login Screen',
+    (WidgetTester tester) async {
+      // Create the widget by telling the tester to build it.
+      Widget testWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(
+          home: new LoginScreen(),
+        ),
+      );
+      await tester.pumpWidget(testWidget);
+
+      // Create the Finders.
+      final emailFinder = find.text('Email Address');
+      final passwordFinder = find.text('Password');
+
+      expect(emailFinder, findsOneWidget);
+      expect(passwordFinder, findsOneWidget);
+    },
+  );
+}
